76d331ccf7c3c8ac11ac5933cf9e53a81555e516
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-10-21 15:36:24 -0400

    feature:[enable cors by any domain]

--- api/src/main.ts
+++ api/src/main.ts
@@ -22,6 +22,14 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.enableCors({
+    origin: ['*'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
