ed37f4258e17537900f73ae9539e0519c01dc0da
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-10-21 15:34:22 -0400

    feature:[add swagger and validation pipes dependencies]

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,27 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Test PulpoLine')
+    .setDescription('API for the Test Pulpo Line')
+    .setVersion('1.0')
+    .addTag('PulpoLine')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
